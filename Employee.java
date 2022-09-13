e1855eb50ff8d885a89fe89984ed0fd9bd2a23bb
monikamemane7 <113007091+monikamemane7@users.noreply.github.com>
2022-09-13 10:31:13 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,12 @@
+class Employee
+{
+static String company="vedanta";
+static String manager="manager";
+int id;
+String name;
+
+void employeeInfo()
+{
+System.out.println("company name:"+company+"under:"+manager+"Employee id:"+id+"Employee name:"+name);
+}
+}
---
+++ MainEmployee.java
@@ -0,0 +1,22 @@
+class MainEmployee
+{
+public static void main(String args[])
+{
+Employee e1 =new Employee();
+Employee e2 =new Employee();
+Employee e3 =new Employee();
+
+e1.id=1;
+e1.name="monika";
+
+e2.id=2;
+e2.name="vikram";
+
+e3.id=3;
+e3.name="adarsh";
+
+e1.employeeInfo();
+e2.employeeInfo();
+e3.employeeInfo();
+}
+}
